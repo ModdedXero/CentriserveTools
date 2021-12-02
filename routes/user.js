75866d688a28de44d9aae1d619158194981ac3c3
ModdedXero <53191205+ModdedXero@users.noreply.github.com>
2021-12-01 17:24:58 -0700

    Fixed typos in users route

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,15 @@
 const router = require("express").Router();
 
-const mongo = require("../database/mongo");
+const auth = require("../database/auth");
 
 router.route("/login").post(async (req, res) => {
-    const options = {
-        httpOnly: true,
-        signed: true
-    }
-
-    const result = await mongo.ValidateLogin(req.body.username, req.body.password ? req.body.password : "");
+    const result = await auth.ValidateLogin(req.body.username, req.body.password ? req.body.password : "");
     switch (result) {
         case 3:
             res.status(200).json({ response: "bad_password" });
             break;
         case 2:
-            res.status(200).json({ response: "create_password"});
+            res.status(200).json({ response: "create_password" });
             break;
         case 1:
             res.status(200).json({ response: "authenticated", token: { username: req.body.username, auth: "approved" } });
@@ -23,12 +18,12 @@ router.route("/login").post(async (req, res) => {
             res.status(200).json({ response: "not_authenticated" });
             break;
         default:
-            res.status(2000).json({ response: "not_authenticated" });
+            res.status(200).json({ response: "not_authenticated" });
     }
 });
 
 router.route("/password").post(async (req, res) => {
-    const result = await mongo.SavePassword(req.body.username, req.body.password);
+    const result = await auth.SavePassword(req.body.username, req.body.password);
 
     if (result) {
         res.status(200).json({ response: "password_saved", token: { username: req.body.username, auth: "approved" }});
