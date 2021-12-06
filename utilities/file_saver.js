07ed1ff3d46b51fad754b5520c86eaa10fb721c8
ModdedXero <53191205+ModdedXero@users.noreply.github.com>
2021-12-06 09:41:45 -0700

    Added loging to file saver

--- utilities/file_saver.js
+++ utilities/file_saver.js
@@ -7,8 +7,8 @@ const repoPath = process.platform === "win32"
 
 async function WriteFile(fileName, data, type) {
     let result = false;
-    await fs.mkdir(type + "\\", { recursive: true });
-    await fs.writeFile(`${type}/${fileName}`, data, "utf-8")
+    await fs.mkdir(type, { recursive: true });
+    await fs.writeFile(`${type}\\${fileName}`, data, "utf-8")
             .then(data => { result = true; })
 
     return result;
@@ -21,9 +21,9 @@ async function ReadFile(fileName, type) {
     console.log(path.dirname(__dirname));
     console.log(path.basename(path.dirname(__dirname)));
     console.log(path.dirname(path.basename(path.dirname(__dirname))))
-    await fs.access(`${type}/${fileName}`)
+    await fs.access(`${type}\\${fileName}`)
         .then(async err => {
-            rFile = await fs.readFile(`${type}/${fileName}`, "utf-8")
+            rFile = await fs.readFile(`${type}\\${fileName}`, "utf-8")
         })
         .catch(err => {
             rFile = undefined;
@@ -35,7 +35,7 @@ async function ReadFile(fileName, type) {
 async function IsFile(fileName, type) {
     let file = "";
 
-    await fs.access(`${type}/${fileName}`)
+    await fs.access(`${type}\\${fileName}`)
             .then(_ => {
                 file = `${type}\\${fileName}`;
             })
@@ -49,7 +49,7 @@ async function IsFile(fileName, type) {
 async function ModifiedDate(fileName, type) {
     let date = undefined;
 
-    await fs.stat(`${type}/${fileName}`)
+    await fs.stat(`${type}\\${fileName}`)
             .then(data => {
                 date = data.mtime;
             })
